61828a2262452fc21d3bc4883fba14ddd3196f0d
Sergiu Corfu <sergiucorfu@yahoo.com>
2020-11-16 19:31:50 +0200

    showPages without comments

--- functions.js
+++ functions.js
@@ -20,31 +20,3 @@ function showPage(pageId){
     hideAllPages();
     document.getElementById(pageId).style.display = '';  
 }
-
-
-// function showHome(){
-//     showPage('home');
-//     // hideAllPages();
-//     // document.getElementById('home').style.display = '';  
-// }
-
-// function showSkills(){
-//     showPage('skills');
-//     // hideAllPages();
-//     // document.getElementById('skills').style.display = "";  
-// }
-
-//  function showProjects(){
-//     showPage('projects');
-//     // hideAllPages();
-//     // document.getElementById('projects').style.display = "";
-// }
-// function showLanguages(){
-//     showPage('languages')
-//     // hideAllPages();
-//     // // document.getElementById('home').style.display = 'none';
-//     // // document.getElementById("skills").style.display = "none";
-//     // // document.getElementById("projects").style.display = "none";
-//     // document.getElementById('languages').style.display = "";
-// }
-
