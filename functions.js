a099370db923c86122f0878b36e2118e8da0aa50
Sergiu Corfu <sergiucorfu@yahoo.com>
2020-12-07 21:13:24 +0200

    simplified using arrow fn

--- functions.js
+++ functions.js
@@ -25,7 +25,7 @@ function showPage(pageId){
 }
 
 function initMenu() {
-    document.addEventListener('click', function(e){
+    document.addEventListener('click', (e) => {
         const link = e.target
         if (link.matches('#top-menu-bar a')) {
             const id = link.getAttribute('data-page');
@@ -39,7 +39,7 @@ initMenu();
 showPage(activePage);
 
 function getHTMLSkills(skills) {
-    return skills.map(function(skill){
+    return skills.map(skill => {
         return `<li class="${skill.endorsements > 9 ? "favorite" : ""}"> 
         ${skill.name} <span>&middot; ${skill.endorsements}</span>
         </li>`;
@@ -53,10 +53,10 @@ function showSkills (skills) {
 }
 
 
-fetch("data/skills.json").then(function(r) {
+fetch("data/skills.json").then( (r) => {
     return r.json();
-}).then(function(allSkills) {
-    allSkills.sort(function(s1, s2) {
+}).then( (allSkills) => {
+    allSkills.sort( (s1, s2) => {
         return s2.endorsements - s1.endorsements;
         // return s1.name < s2.name ? -1 : 0;
     } );
